0fb8b453e37a6eb956a7b95b12a0c19ac6a3883d
Sharpererer <143591438+Sharpererer@users.noreply.github.com>
2023-08-30 16:36:12 +0700

    Fix 1.1

--- 111 to date/date.py
+++ 111 to date/date.py
@@ -1,23 +1,24 @@
 day = int(input('Enter days: '))
-year = 0
+year = 1
 
 
-while day - 365 > 0 or day - 366 > 0 :
-    if year + 1 % 100 == 0:
+while day - 365 > 0 or day - 366 > 0:
+    if year == 1:
+        day -= 364
+    elif year % 100 == 0:
         day -= 366 if year % 400 == 0 else 365
     else:
         day -= 366 if year % 4 == 0 else 365
     year += 1
 
-months = [31,29,31,30,31,30,31,31,30,31,30,31] if (year + 1) % 4 ==0 else [31,28,31,30,31,30,31,31,30,31,30,31]
+months = [31,29,31,30,31,30,31,31,30,31,30,31] if year % 4 == 0 else [31,28,31,30,31,30,31,31,30,31,30,31]
 
 for i in range(0,12):
     day -= months[i]
-    if day < 0:
-        day += months[i] + 1
+    if day < 1:
+        day += months[i]
         break
 
 month = i + 1
 
-
-print(day,month,year + 1)
+print(day,month,year)
